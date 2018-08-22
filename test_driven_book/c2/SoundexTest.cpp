22f861e2b95aab7601e0f13cbf885ddae1bb3d94
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2018-08-22 14:59:25 -0700

    Add default destructor

--- test_driven_book/c2/SoundexTest.cpp
+++
@@ -1,6 +0,0 @@
-#include "gmock/gmock.h"    //<label id="code.include"/>
-
-TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) { //<label id="code.test"/>
-   Soundex soundex;   //<label id="code.construct"/>
-}
-
